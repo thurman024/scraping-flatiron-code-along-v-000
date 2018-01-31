13d8066c48e2b377ab244871f10ef4a91f6a3c14
Stephen Thurman <thurman024@gmail.com>
2018-01-31 23:28:57 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -16,6 +16,7 @@ class Scraper
   def get_courses
     doc.css(".post").first.css("h2").text
     doc.css(".post").first.css(".date").text
+    doc.css(".post").first.css("p").text
   end
 
   def make_courses

1cf7bb140cb2012621d8946c89d531e9a1b3b459
Stephen Thurman <thurman024@gmail.com>
2018-01-31 23:38:57 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -20,9 +20,9 @@ class Scraper
   def make_courses
     self.get_courses.each do |post|
       course = Course.new
-      course.title = post.first.css("h2").text
-      course.schedule = post.first.css(".date").text
-      course.description = post.first.css("p").text
+      course.title = post.css("h2").text
+      course.schedule = post.css(".date").text
+      course.description = post.css("p").text
     end
   end
 

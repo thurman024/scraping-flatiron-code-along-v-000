8c11f917b01e61e817171ffdb6f0daa112363f5e
Stephen Thurman <thurman024@gmail.com>
2018-01-31 23:03:41 +0000

    automatically backed up by learn

--- lib/course.rb
+++ lib/course.rb
@@ -1,8 +1,17 @@
 class Course
   attr_accessor :title, :schedule, :description
-  @@courses = []
-  def all
-    @@courses
+  @@all = []
+
+  def initialize
+    @@all << self
+  end
+
+  def self.reset_all
+    @@all.clear
+  end
+  
+  def self.all
+    @@all
   end
 
 end
